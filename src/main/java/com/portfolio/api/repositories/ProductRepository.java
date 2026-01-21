9cfbdd82f658e5716c8405cf9547f1217cca2e7e
imaisilva <alohasp@outlook.com>
2026-01-21 13:59:17 -0300

    product entity

---
+++ src/main/java/com/portfolio/api/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.portfolio.api.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.portfolio.api.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long>{
+
+}
